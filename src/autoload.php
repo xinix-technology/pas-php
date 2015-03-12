eb653010f1786efe5e1ccf05994d9c1866acd0c3
Ganesha <reekoheek@gmail.com>
2015-03-12 09:54:29 +0700

    Fixed autoload to accept exact string namespace

--- src/autoload.php
+++ src/autoload.php
@@ -5,11 +5,16 @@ $autoload = json_decode(file_get_contents('../autoload.json'), 1);
 spl_autoload_register(function ($class) use ($autoload) {
     foreach ($autoload as $ns => $loader) {
         if ($class === $ns || strpos($class, $ns.'\\') === 0) {
+
+            $fileSuffix = '';
+            if ($class !== $ns) {
+                $fileSuffix = DIRECTORY_SEPARATOR . preg_replace('/\\\/', '/', substr($class, strlen($ns) + 1));
+            }
+
             $file = '..'
                 . DIRECTORY_SEPARATOR
                 . $loader
-                . DIRECTORY_SEPARATOR
-                . preg_replace('/\\\/', '/', substr($class, strlen($ns) + 1))
+                . $fileSuffix
                 . '.php';
 
             if (stream_resolve_include_path($file)) {
